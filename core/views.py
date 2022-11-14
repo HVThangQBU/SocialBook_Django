fcee0977450a46e19172f572e9e2e5d81fa81170
hoangthang870 <57425057+HVThangQBU@users.noreply.github.com>
2022-11-14 22:35:47 +0700

    commit signup

--- core/views.py
+++ core/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import auth,User
 
 def index(request):
   return render(request, 'index.html')
 
+def signup(request):
+ if request.method == 'POST':
+   username = request.POST['username']
+   email = request.POST['email']
+   password = request.POST['password']
+   password2 = request.POST['password2']
+   if password == password2:
+     if User.objects.filter(email=email).exists():
+       messages.info(request,'EMAIL TAKENS')
+       return redirect('signup')
+     elif User.objects.filter(username=username).exists():
+       messages.info(request,'USERNAME TAKENS')
+       return redirect('signup')
+     else:
+       user = User.objects.create_user(username=username, email=email, password=password)
+       user.save()
+       # log user in and redirectto setting page
+       # create a profile object for the new user
+       user_model = User.objects.get(username=username)
+     return redirect('index')
+   else:
+    messages.info(request,'PASS NOT MATCHING')
+    return redirect('signup')
+
+ else:
+  return render(request, 'signup.html')
